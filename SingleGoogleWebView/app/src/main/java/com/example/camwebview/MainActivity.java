18442601361f6173c96de511c70ccae02eb79909
Seran An <56205657+seran1001@users.noreply.github.com>
2020-06-24 15:21:22 +0900

    A Single google WebView for practice generating module
    1. Implement Google payment Module
    2. Image Recognition payment Module

---
+++ SingleGoogleWebView/app/src/main/java/com/example/camwebview/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.camwebview;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        webView = (WebView) findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://www.google.com");
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+    }
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
