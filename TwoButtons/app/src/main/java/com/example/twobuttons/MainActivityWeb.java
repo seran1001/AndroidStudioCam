497b678dab943b38953323733bbebb1c966c5e4c
Seran An <56205657+seran1001@users.noreply.github.com>
2020-06-25 18:15:47 +0900

    Two Buttons_updated with Iamport payment module

--- TwoButtons/app/src/main/java/com/example/twobuttons/MainActivityWeb.java
+++ TwoButtons/app/src/main/java/com/example/twobuttons/MainActivityWeb.java
@@ -3,6 +3,9 @@ package com.example.twobuttons;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.ActivityNotFoundException;
+import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -19,14 +22,22 @@ import android.webkit.WebView;
 
 import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.URISyntaxException;
+
 public class MainActivityWeb extends AppCompatActivity {
     private WebViewJavaScriptInterface webViewJavaScriptInterface;
     private WebView webView;
+
+    private final String APP_SCHEME = "iamportapp";
+
+
     /**
      * Read a file from the assets folder to a string.
+     *
      * @param fileName includes the file path starting with the assets directory as the root
      * @return String
      */
@@ -58,6 +69,53 @@ public class MainActivityWeb extends AppCompatActivity {
         return stringBuilder.toString();
     }
 
+  //  @Override
+    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+
+        if (!url.startsWith("http://") && !url.startsWith("https://") && !url.startsWith("javascript:")) {
+            Intent intent = null;
+
+            try {
+                intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME); //IntentURI처리
+                Uri uri = Uri.parse(intent.getDataString());
+
+                startActivity(new Intent(Intent.ACTION_VIEW, uri)); //해당되는 Activity 실행
+                return true;
+            } catch (URISyntaxException ex) {
+                return false;
+            } catch (ActivityNotFoundException e) {
+                if (intent == null) return false;
+
+                if (handleNotFoundPaymentScheme(intent.getScheme()))
+                    return true; //설치되지 않은 앱에 대해 사전 처리(Google Play이동 등 필요한 처리)
+
+                String packageName = intent.getPackage();
+                if (packageName != null) { //packageName이 있는 경우에는 Google Play에서 검색을 기본
+                    startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + packageName)));
+                    return true;
+                }
+
+                return false;
+            }
+        }
+
+        return false;
+    }
+
+    private boolean handleNotFoundPaymentScheme(String scheme) {
+        return true;
+    }
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        String url = intent.toString();
+
+        if (url.startsWith(APP_SCHEME)) {
+            // "iamportapp://https://pgcompany.com/foo/bar"와 같은 형태로 들어옴
+            String redirectURL = url.substring(APP_SCHEME.length() + "://".length());
+            webView.loadUrl(redirectURL);
+        }
+    }
     /**
      * Open a new activity window.
      */
@@ -112,5 +170,6 @@ public class MainActivityWeb extends AppCompatActivity {
 
         // Call a JavaScript function from the native app.
         webView.loadUrl("javascript:alert('This alert function was called from the native app.')");
+
     }
 }
